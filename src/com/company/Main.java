f69902ab1d01887ee38dd9fbccb39a8c4bedb48a
niema ahmed <niemawidaha@ac.c4q.nyc>
2017-10-08 19:37:11 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JavaBank letsGo = new JavaBank();
+        //letsGo.javaTrainer();
+        letsGo.startGame();
+
+    }
+}
